f2ee2618303ad59397fa504a4e5d618e3bfa719a
CHEEPEE <kennethjiepadasas@gmail.com>
2017-10-09 03:07:40 +0800

    fixed msg title

--- SchoolAnnouncementGroupChatWithFileSharing/app/src/main/java/com/announcement/schol/schoolannouncementgroupchatwithfilesharing/GroupChatActivity.java
+++ SchoolAnnouncementGroupChatWithFileSharing/app/src/main/java/com/announcement/schol/schoolannouncementgroupchatwithfilesharing/GroupChatActivity.java
@@ -232,27 +232,6 @@ public class GroupChatActivity extends AppCompatActivity {
 
 
     }
-    public void performImageSearch() {
-
-        // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
-        // browser.
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-
-        // Filter to only show results that can be "opened", such as a
-        // file (as opposed to a list of contacts or timezones)
-        //intent.addCategory(Intent.CATEGORY_OPENABLE);
-
-        // Filter to show only images, using the image MIME data type.
-        // If one wanted to search for ogg vorbis files, the type would be "audio/ogg".
-        // To search for all documents available via installed storage providers,
-        // it would be "*/*".
-        intent.setType("image/*");
-
-
-        startActivityForResult(Intent.createChooser(intent,"Choose File"), READ_REQUEST_CODE);
-
-
-    }
     @Override
     public void onActivityResult(int requestCode, int resultCode,
                                  Intent resultData) {
@@ -299,10 +278,11 @@ public void uploadFileFromInputStream(final Uri uri){
 
     final String msgTitle;
 
-    if (msgField.getText().toString().equals("") || msgField.getText().toString().equals(null)){
+    if (msgField.getText().toString().trim().equals("") || msgField.getText().toString().equals(null)){
         msgTitle = getFileName(uri);
     }else {
         msgTitle = msgField.getText().toString();
+
     }
 
 
